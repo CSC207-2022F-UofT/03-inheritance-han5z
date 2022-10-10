7be268033168b41cacc39a07c87e1c8312d456f6
han5z <lexmic2015@gmail.com>
2022-10-09 22:58:24 -0400

    Second commit

--- src/main/java/Bag.java
+++ src/main/java/Bag.java
@@ -34,7 +34,7 @@ public abstract class Bag {
         this.color = color;
         this.numberOfContents = 0;
         this.capacity = capacity;
-        this.contents = new String[capacity];
+        this.contents = new String[this.capacity];
     }
 
 
